ecc7da77d1abc34f7e59de3c0de5b989dfc8d9aa
Francesc Lapedriza <francesc.lapedriza@immfly.com>
2023-02-26 21:10:01 +0100

    Optimize models

--- bot/db/models.py
+++ bot/db/models.py
@@ -7,9 +7,9 @@ from sqlmodel import SQLModel, Field, Relationship
 class Person(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(unique=True, index=True)
-    channel_id: str = Field(unique=True, index=True)
-    is_admin: bool = False
-    wants_prompts: Optional[bool] = None
+    channel_id: str = Field(unique=True)
+    is_admin: bool = Field(default=False, index=True)
+    wants_prompts: bool = Field(default=False, index=True)
     assistances: list["PersonAssistance"] = Relationship(back_populates="person")
 
 
@@ -17,7 +17,7 @@ class PersonAssistance(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     person_id: int = Field(foreign_key="person.id")
     person: Person = Relationship(back_populates="assistances")
-    day: date
+    day: date = Field(index=True)
     brought_lunch: bool = Field(default=False, index=True)
     lunch_outside: bool = Field(default=False, index=True)
     in_for_beers: bool = Field(default=False, index=True)
