04c2d9d35e9e09ab5a5c8eebc9b6774040008991
Sergio Garcia Campderrich <gio8.tisu@gmail.com>
2023-02-28 18:38:20 +0100

    Add tomorrow actions

--- bot/plugins/office_attendance.py
+++ bot/plugins/office_attendance.py
@@ -1,5 +1,5 @@
 import re
-from datetime import datetime
+from datetime import datetime, date, timedelta
 from typing import Optional
 
 from mmpy_bot import Plugin, listen_to, Message
@@ -62,6 +62,20 @@ class OfficeAttendancePlugin(Plugin):
         response = await self.action_controller.perform_date_action(message, delete, action)
         self.driver.create_post(message.channel_id, response)
 
+    @listen_to(r'^I will (not )?([a-z ]+) tomorrow$', re.IGNORECASE, direct_only=True)
+    async def tomorrow_action(self, message: Message, not_match: Optional[str], action: str) -> None:
+        delete = not_match is not None
+        action_conversion = {
+            'go to the office': 'in the office',
+            'bring lunch': 'bringing lunch',
+            'eat out': 'eating out',
+            'be in for beers': 'in for beers',
+        }
+        action = action_conversion.get(action, action)
+        action_date_object = date.today() + timedelta(days=1)
+        response = await self.action_controller.perform_date_action(message, delete, action, action_date_object)
+        self.driver.reply_to(message, response)
+
     @listen_to(r'^I will (not )?([a-z ]+) on ([0-9]{2}\/[0-9]{2}\/[0-9]{4})$', re.IGNORECASE, direct_only=True)
     async def future_action(self, message: Message, not_match: Optional[str], action: str, action_date: str) -> None:
         delete = not_match is not None
@@ -79,6 +93,23 @@ class OfficeAttendancePlugin(Plugin):
     @listen_to(r'^Who ([a-z ]+) today\??$', re.IGNORECASE, needs_mention=True)
     async def list_people_action(self, message: Message, action: str) -> None:
         response = await self.action_controller.get_list_of_people_for_action(action)
+        self.driver.create_post(message.channel_id, response)
+
+    @listen_to(
+        r'^Who will ([a-z ]+) tomorrow\??$',
+        re.IGNORECASE,
+        needs_mention=True
+    )
+    async def list_people_action_tomorrow(self, message: Message, action: str) -> None:
+        action_conversion = {
+            'be in the office': 'is in the office',
+            'bring lunch': 'brought lunch',
+            'eat out': 'eats out',
+            'be in for beers': 'is in for beers',
+        }
+        action = action_conversion.get(action, action)
+        action_date_object = date.today() + timedelta(days=1)
+        response = await self.action_controller.get_list_of_people_for_action(action, action_date_object)
         self.driver.create_post(message.channel_id, response)
 
     @listen_to(
